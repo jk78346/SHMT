2d11365ea4e1d7b076e38b0f5bfd27cd2cd39b29
khsu037 <jk78346@gmail.com>
2022-08-29 14:58:51 -0700

    update docker_setup script to enable tf GPU support within dockercontainer

--- scripts/docker_setup.sh
+++ scripts/docker_setup.sh
@@ -5,17 +5,17 @@ DATASET_DIR=/nfshome/khsu037/ILSVRC
 TARGET_DIR=/mnt # the dataset mount point within container
 IMAGE_NAME=${PROJ}_image
 CONTAINER_NAME=${PROJ}_container
-DOCKERFILE_PATH=./../samples/0_Simple/GPGTPU_conv_blocking
+DOCKERFILE_PATH=./../src
 
 # build dockerfile to generate docker image
 echo "[${PROJ}] - building docker image from dockerfile..."
 sudo docker build -t ${IMAGE_NAME} ${DOCKERFILE_PATH}
 
-# generate container from image
 docker stop ${CONTAINER_NAME}
 docker rm ${CONTAINER_NAME}
 
+# generate container from image
 # mount dataset dir (ImageNet) from host fs to container fs
 echo "[${PROJ}] - build docker container and mouting dataset dir to container..."
-sudo docker run -d -it --name ${CONTAINER_NAME} --mount type=bind,source=${DATASET_DIR},target=${TARGET_DIR} ${IMAGE_NAME} bash
+sudo docker run -d -it --gpus all --name ${CONTAINER_NAME} --mount type=bind,source=${DATASET_DIR},target=${TARGET_DIR} ${IMAGE_NAME} bash
 
