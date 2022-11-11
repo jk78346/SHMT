64344172e706356768edff286633d76238a584e9
khsu037 <jk78346@gmail.com>
2022-11-10 22:37:58 -0800

    separated out data initialization as a function, need to consider how to design input/output types for applications.

--- src/main.cpp
+++ src/main.cpp
@@ -74,20 +74,20 @@ float run_kernel_on_gpu(std::string app_name, cuda::GpuMat& img, float* output_a
     return get_time_ms(end, start);
 }
 
-float run_kernel(const std::string& mode, Params& params, float* input_array, float* output_array, int iter){
+float run_kernel(const std::string& mode, Params& params, float* input_array, float* output_array){
     float kernel_ms = 0.0;
     std::cout << __func__ << ": start running kernel in " << mode << " mode" 
-              << " with iter = " << iter << std::endl;
+              << " with iter = " << params.iter << std::endl;
     if(mode == "cpu"){
         Mat img;
         array2mat(img, input_array, CV_32F, params.problem_size, params.problem_size);
-        kernel_ms = run_kernel_on_cpu(params.app_name, img, output_array, iter);        
+        kernel_ms = run_kernel_on_cpu(params.app_name, img, output_array, params.iter);        
     }else if(mode == "gpu"){
 	    Mat img_host;
         array2mat(img_host, input_array, CV_32F, params.problem_size, params.problem_size);
 	    cuda::GpuMat img;
 	    img.upload(img_host); // convert from Mat to GpuMat
-	    kernel_ms = run_kernel_on_gpu(params.app_name, img, output_array, iter);        
+	    kernel_ms = run_kernel_on_gpu(params.app_name, img, output_array, params.iter);        
     }else if(mode == "tpu"){
         int in_size  = params.problem_size * params.problem_size;
         int out_size = params.problem_size * params.problem_size;
@@ -97,7 +97,7 @@ float run_kernel(const std::string& mode, Params& params, float* input_array, fl
             in[i] = ((int)(input_array[i] + 128)) % 256; // float to int conversion
         }
         std::string kernel_path = get_edgetpu_kernel_path(params.app_name, params.problem_size, params.problem_size);
-        run_a_model(kernel_path, params.iter, in, in_size, out, out_size, iter);
+        run_a_model(kernel_path, params.iter, in, in_size, out, out_size, params.iter);
         for(int i = 0 ; i < out_size ; i++){
             output_array[i] = out[i]; // int to float conversion
         }
@@ -109,19 +109,46 @@ float run_kernel(const std::string& mode, Params& params, float* input_array, fl
     return kernel_ms;
 }
 
+void data_initialization(Params params, 
+                         float** input_array,
+                         float** output_array_baseline,
+                         float** output_array_proposed){
+    // TODO: choose cooresponding initial depends on app_name.
+    //if(params.app_name == ""){
+    //
+    //}
+    
+    int rows = params.problem_size;
+    int cols = params.problem_size;
+    unsigned int input_total_size = rows * cols;
+    *input_array = (float*) malloc(input_total_size * sizeof(float));
+    
+    unsigned int output_total_size = rows * cols;
+    *output_array_baseline = (float*) malloc(output_total_size * sizeof(float));
+    *output_array_proposed = (float*) malloc(output_total_size * sizeof(float));
+    Mat in_img;
+    read_img(params.input_data_path, 
+             rows, 
+             cols, 
+             in_img);
+    mat2array(in_img, *input_array);
+}
+    
 int main(int argc, char* argv[]){
     if(argc != 6){
         std::cout << "Usage: " << argv[0] 
                   << " <application name> <problem_size> <iter> <baseline mode> <proposed mode>" 
                   << std::endl;
         return 0;
+    }else{
+        // print program arguments
+        for(int i = 0 ; i < argc ; i++){
+            std::cout << argv[i] << " ";
+        }
+        std::cout << std::endl;
     }
-    // print program arguments
-    for(int i = 0 ; i < argc ; i++){
-        std::cout << argv[i] << " ";
-    }
-    std::cout << std::endl;
 
+    // program arguments assignment
     std::string app_name = argv[1];
     int problem_size     = atoi(argv[2]);
     int iter             = atoi(argv[3]);
@@ -133,24 +160,16 @@ int main(int argc, char* argv[]){
                   problem_size/*block size*/, 
                   iter);
 
-    int rows = params.problem_size;
-    int cols = params.problem_size;
-    unsigned int input_total_size = rows * cols;
-    float* input_array = (float*) malloc(input_total_size * sizeof(float));
-    
-    unsigned int output_total_size = rows * cols;
-    float* output_array_baseline = (float*) malloc(output_total_size * sizeof(float));
-    float* output_array_proposed = (float*) malloc(output_total_size * sizeof(float));
+    float* input_array = NULL;
+    float* output_array_baseline = NULL;
+    float* output_array_proposed = NULL;
 
-    Mat in_img;
-    Mat out_img;
+    // input/output array allocation
+    data_initialization(params, 
+                        &input_array,
+                        &output_array_baseline,
+                        &output_array_proposed);
     
-    read_img(params.input_data_path, 
-             rows, 
-             cols, 
-             in_img);
-    mat2array(in_img, input_array);
-
     // peak input image for a cropped section
     std::cout << __func__ << ": peaking input image data..." << std::endl; 
     for(int i = 0 ; i < 5 ; i++){
@@ -168,8 +187,7 @@ int main(int argc, char* argv[]){
     baseline_kernel_ms = run_kernel(baseline_mode, 
                                     params, 
                                     input_array, 
-                                    output_array_baseline,
-                                    iter);
+                                    output_array_baseline);
     timing baseline_end = clk::now();
     
     timing proposed_start = clk::now();
@@ -177,8 +195,7 @@ int main(int argc, char* argv[]){
     proposed_kernel_ms = run_kernel(proposed_mode, 
                                     params, 
                                     input_array, 
-                                    output_array_proposed,
-                                    iter);
+                                    output_array_proposed);
     timing proposed_end = clk::now();
     
     // Calculate end to end latency of each implementation
