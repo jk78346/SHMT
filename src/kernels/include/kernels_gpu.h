0e91967a202b1c367fa20aeb0fe3b005ce07aa61
jk78346 <jk78346@gmail.com>
2022-11-17 12:43:47 -0800

    [working] CMake change: Let kkernel becomes a shared library as a separated part of out main source code to avoid re-compiliation everytime when main source is compiling.

--- src/kernels/include/kernels_gpu.h
+++ src/kernels/include/kernels_gpu.h
@@ -1,5 +1,5 @@
-#ifndef __KERNELS_CPU_H__
-#define __KERNELS_CPU_H__
+#ifndef __KERNELS_GPU_H__
+#define __KERNELS_GPU_H__
 #include <iostream>
 #include <unordered_map>
 #include <opencv2/opencv.hpp>
@@ -10,55 +10,55 @@
 
 using namespace cv;
 
-/* CPU kernel class 
+/* GPU kernel class 
 TODO: optimize function table searching algorithm.
 */
-class CpuKernel : public KernelBase{
+class GpuKernel : public KernelBase{
 public:
-    CpuKernel(Params params, void* input, void* output){
+    GpuKernel(Params params, void* input, void* output){
         this->params = params;
         this->input_array_type.ptr = input;
         this->output_array_type.ptr = output;
-    }
+    };
 
-    virtual ~CpuKernel(){};
-    
+    virtual ~GpuKernel(){};
+
     /* input conversion - search over func_tables to do correct input conversion */
     virtual double input_conversion(){
         timing start = clk::now();
-        std::string app_name = this->params.app_name;
-        if(if_kernel_in_table(this->func_table_cv, app_name)){
-            float* input_array  = 
-                reinterpret_cast<float*>(this->input_array_type.ptr);
-            array2mat(this->input_array_type.mat, 
+        std::string app_name = params.app_name;
+        if(if_kernel_in_table(this->func_table_cv_cuda, app_name)){
+            float* input_array  = reinterpret_cast<float*>(this->input_array_type.ptr);
+            Mat img_host;
+            array2mat(img_host, 
                       input_array, 
                       CV_32F, 
                       this->params.block_size, 
                       this->params.block_size);
+            this->input_array_type.gpumat.upload(img_host); // convert from Mat to GpuMat
         }else if(if_kernel_in_table(this->func_table_fp, app_name)){
-            float* input_array  = 
-                reinterpret_cast<float*>(this->input_array_type.ptr);
-            float* output_array = 
-                reinterpret_cast<float*>(this->output_array_type.ptr);
+            float* input_array  = reinterpret_cast<float*>(this->input_array_type.ptr);
+            float* output_array = reinterpret_cast<float*>(this->output_array_type.ptr);
             this->input_array_type.fp  = input_array;
-            this->output_array_type.fp = output_array;
+            this->output_array_type.fp = output_array; 
         }else{
             // app_name not found in any table. 
         }
         timing end = clk::now();
         return get_time_ms(end, start);
-    }   
-
+    }
+    
     /* output conversion - search over func_tables to do correct output conversion */
     virtual double output_conversion(){
         timing start = clk::now();
-        std::string app_name = this->params.app_name;
-        if(if_kernel_in_table(this->func_table_cv, app_name)){
+        std::string app_name = params.app_name;
+        if(if_kernel_in_table(this->func_table_cv_cuda, app_name)){
             float* output_array = 
                 reinterpret_cast<float*>(this->output_array_type.ptr);
-            this->output_array_type.mat.convertTo(this->output_array_type.mat, 
-                                                  CV_8U);
-            mat2array(this->output_array_type.mat, output_array);
+            Mat out_img;
+            this->output_array_type.gpumat.download(out_img); // convert GpuMat to Mat
+            out_img.convertTo(out_img, CV_8U);
+            mat2array(out_img, output_array);
         }else if(if_kernel_in_table(this->func_table_fp, app_name)){
             // no need to convert from float* to float*, pass
         }else{
@@ -70,8 +70,8 @@ public:
 
     virtual double run_kernel(){
         std::string app_name = this->params.app_name;
-        if(if_kernel_in_table(this->func_table_cv, app_name)){
-            return this->run_kernel_opencv();
+        if(if_kernel_in_table(this->func_table_cv_cuda, app_name)){
+            return this->run_kernel_opencv_cuda();
         }else if(if_kernel_in_table(this->func_table_fp, app_name)){
             return this->run_kernel_float();
         }else{
@@ -80,12 +80,13 @@ public:
         return 0.0; // kernel execution is skipped.
     }
 
+private:
     /* opencv type of input/output */
-    double run_kernel_opencv(){
-        kernel_existence_checking(this->func_table_cv, this->params.app_name);
+    double run_kernel_opencv_cuda(){
+        kernel_existence_checking(this->func_table_cv_cuda, this->params.app_name);
         timing start = clk::now();
-        this->func_table_cv[this->params.app_name](this->input_array_type.mat, 
-                                                   this->output_array_type.mat);
+        this->func_table_cv_cuda[this->params.app_name](this->input_array_type.gpumat, 
+                                           this->output_array_type.gpumat);
         timing end = clk::now();
         return get_time_ms(end, start);
     }
@@ -95,42 +96,41 @@ public:
         kernel_existence_checking(this->func_table_fp, this->params.app_name);
         timing start = clk::now();
         this->func_table_fp[this->params.app_name](this->params, 
-                                                   this->input_array_type.fp, 
-                                                   this->output_array_type.fp);
+                                      this->input_array_type.fp, 
+                                      this->output_array_type.fp);
         timing end = clk::now();
         return get_time_ms(end, start);
     }
-private:
+
     // arrays
     struct ArrayType{
         void* ptr = NULL;
-        Mat mat;
+        cuda::GpuMat gpumat;
         float* fp = NULL;
     };
     Params params;
     ArrayType input_array_type, output_array_type;
     
     // function tables
-    typedef void (*func_ptr_opencv)(const Mat, Mat&); // const Mat: input, Mat& : input/output
+    typedef void (*func_ptr_opencv_cuda)(const cuda::GpuMat, cuda::GpuMat&); // const cuda::GpuMat: input, cuda::GpuMat& : input/output
     typedef void (*func_ptr_float)(Params, float*, float*);
-    typedef std::unordered_map<std::string, func_ptr_opencv> func_table_opencv;
+    typedef std::unordered_map<std::string, func_ptr_opencv_cuda> func_table_opencv_cuda;
     typedef std::unordered_map<std::string, func_ptr_float>  func_table_float;
-    func_table_opencv func_table_cv = {
-        std::make_pair<std::string, func_ptr_opencv> ("minimum_2d", this->minimum_2d),
-        std::make_pair<std::string, func_ptr_opencv> ("sobel_2d", this->sobel_2d),
-        std::make_pair<std::string, func_ptr_opencv> ("mean_2d", this->mean_2d),
-        std::make_pair<std::string, func_ptr_opencv> ("laplacian_2d", this->laplacian_2d)
+    func_table_opencv_cuda func_table_cv_cuda = {
+        std::make_pair<std::string, func_ptr_opencv_cuda> ("minimum_2d", this->minimum_2d),
+        std::make_pair<std::string, func_ptr_opencv_cuda> ("sobel_2d", this->sobel_2d),
+        std::make_pair<std::string, func_ptr_opencv_cuda> ("mean_2d", this->mean_2d),
+        std::make_pair<std::string, func_ptr_opencv_cuda> ("laplacian_2d", this->laplacian_2d)
     };
     func_table_float func_table_fp = {
         std::make_pair<std::string, func_ptr_float> ("fft_2d", this->fft_2d)
     };
 
     // kernels
-    static void minimum_2d(const Mat in_img, Mat& out_img);
-    static void sobel_2d(const Mat in_img, Mat& out_img);
-    static void mean_2d(const Mat in_img, Mat& out_img);
-    static void laplacian_2d(const Mat in_img, Mat& out_img);
+    static void minimum_2d(const cuda::GpuMat in_img, cuda::GpuMat& out_img);
+    static void sobel_2d(const cuda::GpuMat in_img, cuda::GpuMat& out_img);
+    static void mean_2d(const cuda::GpuMat in_img, cuda::GpuMat& out_img);
+    static void laplacian_2d(const cuda::GpuMat in_img, cuda::GpuMat& out_img);
     static void fft_2d(Params params, float* input, float* output);
 };
-
 #endif
