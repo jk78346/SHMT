831975368be7502fe8dca99a3806a94aff5177ec
khsu037 <jk78346@gmail.com>
2022-11-24 12:32:43 -0800

    last adjusting on iter and problem size before actual run

--- scripts/run_test_all.sh
+++ scripts/run_test_all.sh
@@ -4,13 +4,13 @@ GITTOP="$(git rev-parse --show-toplevel 2>&1)"
 BUILD_DIR="./nano_host_build"
 
 cd "${GITTOP}/${BUILD_DIR}"
-iter=1
+iter=100
 baseline_mode="cpu"
 log_file_path="../log/record.csv"
 
 for app_name in "sobel_2d" "mean_2d" "laplacian_2d"
 do
-    for problem_size in 2048 4096 8192
+    for problem_size in 1024 2048 4096 8192
     do
         for block_size in 512 1024 2048
         do
