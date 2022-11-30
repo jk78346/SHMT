516e2f2bc721a0d48707cb7ce707868dd655c69f
khsu037 <jk78346@gmail.com>
2022-11-29 19:57:47 -0800

    work-balancing modes onde and quick tested. The concept of static/dynamic assignment is introduced. Either static device assignment of each tiling block has to be determined before execution stage, or all devices that participate in SPMC scheduling are allowed to consume dynamic tiling blocks.

--- include/partition.h
+++ include/partition.h
@@ -7,13 +7,24 @@
 #include "kernels_cpu.h"
 #include "kernels_gpu.h"
 #include "kernels_tpu.h"
+#include "concurrentqueue.h"
 
 typedef enum _DeviceType { undefine, cpu, gpu, tpu} DeviceType;
 
 typedef struct {
-    KernelBase* kernel_base;
+    KernelBase* kernel_base = NULL;
     DeviceType device_type;
 }GenericKernel;
+
+/*
+    structure of each SPMC FIFO node. 
+ */
+struct node_data{
+    GenericKernel* generic_kernel;
+    Params params;
+    unsigned int block_id;
+    unsigned int iter;
+};
 
 class PartitionRuntime{
 public:
@@ -26,16 +37,47 @@ public:
     double prepare_partitions();
     double run_partitions();
     double transform_output();
-    void create_kernel_by_type(unsigned int block_id, DeviceType device_type);
     void show_device_sequence();
-    unsigned int dev_type_cnt = 3;
+    unsigned int dev_type_cnt = 3; // cpu, gpu and tpu
+
 private:
-    /*
-        The main algorithm to determine tiling tasks to specific device(s). 
-    */
+    /* The main algorithm to determine tiling tasks to specific device(s). */
     DeviceType mix_policy(unsigned int i);
     
-    unsigned int block_cnt = 1;
+    /* threading for each device type. */
+    static void* RunDeviceThread(void* args);
+
+    /* SPMC queue */
+    moodycamel::ConcurrentQueue<struct node_data> q;
+    
+    void create_kernel_by_type(unsigned int block_id, DeviceType device_type);
+
+    /* To determine if each tiling block is static or dynamic
+        by setting the the following arrays:
+        bool* is_dynamic_block
+        bool* any_static_block_per_device
+        bool* is_dynamic_device.
+     */
+    void populate_dynamic_flags();
+
+    /*
+        To indicate if the ith tiling block is under dynamic partitioning mode. 
+        Dynamic block:
+            a tiling block that will only be assigned to a device until 
+            runtime scheduling happens such as SPMC.
+        Static block:
+            a tiling block that can be assigned to a device statically 
+            before execution stage.
+     */
+    bool* is_dynamic_block;
+    
+    /*
+        To indicate if the ith device participates in SPMC scheduling, 
+        which means it's allowed to consume any dynamic tiling block.
+     */
+    bool* is_dynamic_device;
+
+    unsigned int block_cnt = 1; // default no partition
     Params params;
     std::string mode = "cpu_p"; // partition mode, default as cpu_p
     void* input;
@@ -51,4 +93,17 @@ private:
     std::vector<void*> output_pars;
 };
 
+/*
+    pthread arguments
+    Each thread here represents one type of device.
+ */
+struct thread_data{
+    PartitionRuntime* p_run_ptr;
+    GenericKernel* generic_kernels;
+    unsigned int block_cnt;
+    unsigned int iter;
+    double kernel_ms;
+    // The device type the thread is representing.
+    DeviceType device_type;
+};
 #endif
--- src/partition.cpp
+++ src/partition.cpp
@@ -2,83 +2,9 @@
 #include <stdlib.h>
 #include "arrays.h"
 #include "partition.h"
-#include "concurrentqueue.h"
 
-#define SERIAL 0
-#define PTHREAD 1
-#define CONCURRENT_QUEUE 2
-
-#define CONCURRENT PTHREAD //CONCURRENT_QUEUE
-
-#if CONCURRENT == PTHREAD || CONCURRENT == CONCURRENT_QUEUE
-struct thread_data{
-    GenericKernel* generic_kernels;
-    unsigned int block_cnt;
-    unsigned int iter;
-    double kernel_ms;
-    // The device type the thread is representing.
-    DeviceType device_type;
-};
-
-#if CONCURRENT == CONCURRENT_QUEUE
-using namespace moodycamel;
- 
-struct node_data{
-    PartitionRuntime* p_run_ptr;
-    GenericKernel* generic_kernel;
-    Params params;
-    unsigned int block_id;
-    unsigned int iter;
-};
-
-ConcurrentQueue<struct node_data> q;
 std::atomic<int> doneProducers(0);
 std::atomic<int> doneConsumers(0);
-#endif
-
-void *RunDeviceThread(void *my_args){
-    // getting argument(s)
-    struct thread_data *args = (struct thread_data*) my_args;
-    GenericKernel* generic_kernels = args->generic_kernels;
-    unsigned int block_cnt = args->block_cnt;
-    unsigned int iter = args->iter;
-    double kernel_ms = args->kernel_ms;
-    DeviceType device_type = args->device_type;
-    
-    kernel_ms = 0.0;
-#if CONCURRENT == PTHREAD
-    for(unsigned int  i = 0 ; i < block_cnt ; i++){
-        /* Check if the device type this kernel was assigned to is the same as
-           the type this consumer thread is representing. 
-         */
-        if(generic_kernels[i].device_type == device_type){
-            kernel_ms += generic_kernels[i].kernel_base->run_kernel(iter);
-        }
-    }
-#elif CONCURRENT == CONCURRENT_QUEUE
-    struct node_data curr_node;
-    bool itemsLeft;
-    do{
-        itemsLeft = doneProducers.load(std::memory_order_acquire) != 1;
-        while(q.try_dequeue(curr_node)){
-            itemsLeft = true;
-        /*  Start to consume one tiling block.
-            Current implementation has to includes inut conversion overhead 
-            since device type is not determined until now.
-         */
-        unsigned int block_id = curr_node.block_id;
-        curr_node.p_run_ptr->create_kernel_by_type(block_id, device_type);
-        curr_node.generic_kernel->kernel_base->input_conversion();
-        kernel_ms += curr_node.generic_kernel->kernel_base->run_kernel(curr_node.iter);
-        }
-    }while(itemsLeft || 
-                doneConsumers.fetch_add(1, std::memory_order_acq_rel) + 1 == 
-                (int)curr_node.p_run_ptr->dev_type_cnt);
-#endif
-    args->kernel_ms = kernel_ms;
-    pthread_exit(NULL);
-}
-#endif
 
 PartitionRuntime::PartitionRuntime(Params params,
                                    std::string mode,
@@ -92,7 +18,8 @@ PartitionRuntime::PartitionRuntime(Params params,
     this->output = output;
     this->generic_kernels = new GenericKernel[this->block_cnt];
     this->dev_sequence = new DeviceType[this->block_cnt];
-    
+    this->is_dynamic_block = new bool[this->block_cnt]; 
+    this->is_dynamic_device = new bool[this->dev_type_cnt]; 
     // For rand_p partition mode
     srand(time(NULL));
     
@@ -104,37 +31,12 @@ PartitionRuntime::~PartitionRuntime(){
     }   
     delete this->generic_kernels;
     delete this->dev_sequence;
+    delete this->is_dynamic_block;
+    delete this->is_dynamic_device;
     this->input_pars.clear();
     this->output_pars.clear();
 }
 
-void PartitionRuntime::create_kernel_by_type(unsigned int i/*block_id*/, 
-                                             DeviceType device_type){
-    if(device_type == cpu){
-        this->generic_kernels[i].kernel_base =
-            new CpuKernel(this->params,
-                          this->input_pars[i],
-                          this->output_pars[i]);
-        this->generic_kernels[i].device_type = cpu;
-    }else if(device_type == gpu){
-        this->generic_kernels[i].kernel_base =
-            new GpuKernel(this->params,
-                          this->input_pars[i],
-                          this->output_pars[i]);
-        this->generic_kernels[i].device_type = gpu;
-    }else if(device_type == tpu){
-        this->generic_kernels[i].kernel_base =
-            new TpuKernel(this->params,
-                          this->input_pars[i],
-                          this->output_pars[i]);
-        this->generic_kernels[i].device_type = tpu;
-    }else{
-        std::cout << __func__ << ": undefined device type: "
-                  << device_type << ", program exits."
-                  << std::endl;
-        exit(0);
-    }
-}
 
 double PartitionRuntime::prepare_partitions(){
     timing start = clk::now();
@@ -149,44 +51,92 @@ double PartitionRuntime::prepare_partitions(){
                                    true, // skip_init
                                    &(this->output),
                                    this->output_pars);
-/*
-    Work-balancing type of partition modes require device type to be determined
-    at SPMC runtime stage, but not pre-execution stage as here.
- */
-#if CONCURRENT != CONCURENT_QUEUE
-    // assign partitions to each type of kernel handler
+    
+    /* This is the latest moment to determine if each tiling block and device is 
+       dynamic or static. */
+    this->populate_dynamic_flags();
+    
+    // assign partitions to corresponding type of kernel handler if is static.
     for(unsigned int i = 0 ; i < this->block_cnt ; i++){
-        auto device_type = this->mix_policy(i);
-        this->create_kernel_by_type(i, device_type);
-        this->generic_kernels[i].kernel_base->input_conversion();
+        if( !this->is_dynamic_block[i] ){
+            auto device_type = this->mix_policy(i);
+            this->create_kernel_by_type(i, device_type);
+            this->generic_kernels[i].kernel_base->input_conversion();
+        }
     }
-#endif
     timing end = clk::now();
     return get_time_ms(end, start);
 }
 
-double PartitionRuntime::run_partitions(){
-#if CONCURRENT == PTHREAD || CONCURRENT == CONCURRENT_QUEUE
-    timing start = clk::now();
-
-#if CONCURRENT == CONCURRENT_QUEUE
-    /*
-        This version uses SPMC where the main thread is producing tiling blocks
-        and consumers are consuming tasks. Each consumer represents one type of
-        computation hardware including CPU, GPU and edgeTPU.
-    */
-    // producer
-    for(unsigned int i = 0 ; i < this->block_cnt ; i++){
+void* PartitionRuntime::RunDeviceThread(void *my_args){
+    // getting argument(s)
+    struct thread_data *args = (struct thread_data*) my_args;
+    auto p_run_ptr = args->p_run_ptr; // pointer of 'this'
+    GenericKernel* generic_kernels = args->generic_kernels;
+    unsigned int block_cnt = args->block_cnt;
+    unsigned int iter = args->iter;
+    double kernel_ms = args->kernel_ms;
+    DeviceType device_type = args->device_type;
+    
+    kernel_ms = 0.0;
+    
+    // To consume any tiling block that is assigned to this device statically.
+    for(unsigned int i = 0 ; i < block_cnt ; i++){
+        /* Check if the device type this kernel was assigned to is the same as
+           the type this consumer thread is representing. 
+         */
+        if(p_run_ptr->is_dynamic_block[i] == false &&
+            generic_kernels[i].device_type == device_type){
+            kernel_ms += generic_kernels[i].kernel_base->run_kernel(iter);
+        }
+    }
+    
+    // device as dynamic consumer
+    if(p_run_ptr->is_dynamic_device[device_type]){
         struct node_data curr_node;
-        curr_node.p_run_ptr = this;
-        curr_node.generic_kernel = &(this->generic_kernels[i]);
-        curr_node.params = this->params;
-        curr_node.block_id = i;
-        curr_node.iter = this->params.iter;
-        q.enqueue(curr_node);
+        bool itemsLeft;
+        do{
+            itemsLeft = doneProducers.load(std::memory_order_acquire) != 1;
+            while(p_run_ptr->q.try_dequeue(curr_node)){
+                itemsLeft = true;
+            /*  Start to consume one tiling block.
+                Current implementation has to includes inut conversion overhead 
+                since device type is not determined until now.
+            */
+            unsigned int block_id = curr_node.block_id;
+            p_run_ptr->create_kernel_by_type(block_id, device_type);
+            p_run_ptr->dev_sequence[block_id] = device_type;
+            curr_node.generic_kernel->kernel_base->input_conversion();
+            kernel_ms += 
+                curr_node.generic_kernel->kernel_base->run_kernel(curr_node.iter);
+            }
+        }while(itemsLeft || 
+                doneConsumers.fetch_add(1, std::memory_order_acq_rel) + 1 == 
+                (int)p_run_ptr->dev_type_cnt);
+    }
+    args->kernel_ms = kernel_ms;
+    pthread_exit(NULL);
+}
+
+double PartitionRuntime::run_partitions(){
+    timing start = clk::now();
+    /*
+       Dynamic producer of SPMC scheduling.
+       Any dynamic tiling block that is left un-assigned to any device during
+       static assignment stage now will be push into SPMC FIFO queue for 
+       dynamic scheduling.
+    */
+    for(unsigned int i = 0 ; i < this->block_cnt ; i++){
+        if(this->is_dynamic_block[i]){
+            struct node_data curr_node;
+            curr_node.generic_kernel = &(this->generic_kernels[i]);
+            curr_node.params = this->params;
+            curr_node.block_id = i;
+            curr_node.iter = this->params.iter;
+            this->q.enqueue(curr_node);
+        }
     }
     doneProducers.fetch_add(1, std::memory_order_release);
-#endif
 
     //create pthreads for each device as runtime threading
     pthread_t threads[this->dev_type_cnt];
@@ -203,10 +153,11 @@ double PartitionRuntime::run_partitions(){
     
     // create device threads
     for(unsigned int i = 0 ; i < this->dev_type_cnt ; i++){
+        td[i].p_run_ptr = this;
         td[i].generic_kernels = this->generic_kernels; 
         td[i].block_cnt = this->block_cnt;
         td[i].iter = this->params.iter;
-        pthread_create(&threads[i], NULL, RunDeviceThread, (void *)&td[i]);
+        pthread_create(&threads[i], NULL, this->RunDeviceThread, (void *)&td[i]);
     }
 
     // wait for join
@@ -220,15 +171,6 @@ double PartitionRuntime::run_partitions(){
     double e2e_kernel_ms = get_time_ms(end, start);
     std::cout << __func__ << ": e2e kernel time: " << e2e_kernel_ms << " (ms) (pthread overhead included)" << std::endl;
     return e2e_kernel_ms;
-#elif CONCURRENT == SERIAL 
-    double kernel_ms = 0.0;
-    for(unsigned int  i = 0 ; i < this->block_cnt ; i++){
-        kernel_ms += this->generic_kernels[i].kernel_base->run_kernel(this->params.iter);
-    }   
-    return kernel_ms;
-#else // any undefined version of implementation
-    return kernel_ms;
-#endif
 }
 
 double PartitionRuntime::transform_output(){
@@ -243,46 +185,69 @@ double PartitionRuntime::transform_output(){
     return get_time_ms(end, start);
 }
 
+void PartitionRuntime::create_kernel_by_type(unsigned int i/*block_id*/, 
+                                             DeviceType device_type){
+    if(this->generic_kernels[i].kernel_base != NULL){
+        std::cout << "[WARN] " << __func__ << ": generic_kenrels[" << i 
+                  << "] has been instanciated as type " 
+                  << this->generic_kernels[i].device_type 
+                  << ", and now type " << device_type 
+                  << " is wanted. Skip creating." << std::endl;
+    }else{
+        if(device_type == cpu){
+            this->generic_kernels[i].kernel_base =
+                new CpuKernel(this->params,
+                              this->input_pars[i],
+                              this->output_pars[i]);
+            this->generic_kernels[i].device_type = cpu;
+        }else if(device_type == gpu){
+            this->generic_kernels[i].kernel_base =
+                new GpuKernel(this->params,
+                              this->input_pars[i],
+                              this->output_pars[i]);
+            this->generic_kernels[i].device_type = gpu;
+        }else if(device_type == tpu){
+            this->generic_kernels[i].kernel_base =
+                new TpuKernel(this->params,
+                              this->input_pars[i],
+                              this->output_pars[i]);
+            this->generic_kernels[i].device_type = tpu;
+        }else{
+           std::cout << __func__ << ": undefined device type: "
+                     << device_type << ", program exits."
+                     << std::endl;
+           exit(0);
+        }
+    }
+}
+
 DeviceType PartitionRuntime::mix_policy(unsigned i
         /*index of a tiling task, no larger than this->block_cnt*/){
     DeviceType ret = undefine;
-    if(this->mode == "cpu_p"){ // all partitions on cpu
+    if(this->mode == "c_p"){ // all partitions on cpu
         ret = cpu;
-    }else if(this->mode == "gpu_p"){ // all partitions on gpu
+    }else if(this->mode == "g_p"){ // all partitions on gpu
         ret = gpu;
-    }else if(this->mode == "tpu_p"){ // all partitions on tpu
+    }else if(this->mode == "t_p"){ // all partitions on tpu
         ret = tpu;
-    }else if(this->mode == "alls_p"){ // sequentially choose a device between cpu, gpu and tpu
+    }else if(this->mode == "cgt_s"){ // sequentially choose a device between cpu, gpu and tpu
         int idx = i%3;
         ret = (idx == 0)?cpu:((idx == 1)?gpu:tpu);
-    }else if(this->mode == "cgs_p"){ // sequentially choose between cpu and gpu
+    }else if(this->mode == "cg_s"){ // sequentially choose between cpu and gpu
         ret = (i%2 == 0)?cpu:gpu;
-    }else if(this->mode == "gts_p"){ // sequentially choose between gpu and tpu
+    }else if(this->mode == "gt_s"){ // sequentially choose between gpu and tpu
         ret = (i%2 == 0)?gpu:tpu;
-    }else if(this->mode == "cts_p"){ // sequentially choose between cpu and tpu
+    }else if(this->mode == "ct_s"){ // sequentially choose between cpu and tpu
         ret = (i%2 == 0)?cpu:tpu;
-    }else if(this->mode == "allr_p"){ // randomly choose a device among cpu, gpu and tpu
-        int idx = rand()%3;
-        ret = (idx == 0)?cpu:((idx == 1)?gpu:tpu);
-    }else if(this->mode == "cgr_p"){ // randomly choose between cpu and gpu
-        int idx = rand()%2;
-        ret = (idx == 0)?cpu:gpu;
-    }else if(this->mode == "gtr_p"){ // randomly choose between gpu and tpu
-        int idx = rand()%2;
-        ret = (idx == 0)?gpu:tpu;
-    }else if(this->mode == "cgr_p"){ // randomly choose between cpu and tpu
-        int idx = rand()%2;
-        ret = (idx == 0)?cpu:tpu;
-    }else if(this->mode == "allb_p" ||
-             this->mode == "cgb_p" ||
-             this->mode == "gtb_p" ||
-             this->mode == "ctb_p"){
+    }else if(this->mode == "cgt_b" ||
+             this->mode == "cg_b" ||
+             this->mode == "gt_b" ||
+             this->mode == "ct_b"){
         /*
            For work-balancing type of modes, device assignment of each tiling 
-           block is determined by SPMC at runtime. No need to pre-determine
-           here so do nothing but dummy.
+           block is dynamic (determined by SPMC at runtime). No need to 
+           pre-determine here so do nothing.
          */
-        ret = cpu;
     }else{
         std::cout << __func__ << ": undefined partition mode: "
                   << this->mode << ", program exits."
@@ -300,3 +265,37 @@ void PartitionRuntime::show_device_sequence(){
     }   
     std::cout << std::endl;
 }
+
+void PartitionRuntime::populate_dynamic_flags(){
+    unsigned int delimiter_loc = this->mode.find("_");
+    
+    // default as static
+    for(unsigned int i = 0 ; i < this->block_cnt ; i++){
+        this->is_dynamic_block[i] = false;
+    }
+    for(unsigned int i = 0 ; i < this->dev_type_cnt ; i++){
+        this->is_dynamic_device[i] = false;
+    }
+
+    if(delimiter_loc != std::string::npos && 
+        this->mode.length() > delimiter_loc &&
+        this->mode.substr(delimiter_loc+1, 1) == "b"){
+        
+        // switch block to dynamic.
+        for(unsigned int i = 0 ; i < this->block_cnt ; i++){
+            this->is_dynamic_block[i] = true;
+        }
+        // switch device to dynamic if detected.
+        std::string sub_mode = this->mode.substr(0, delimiter_loc);
+        if(sub_mode.find("c") != std::string::npos){ // found cpu type
+            this->is_dynamic_device[cpu] = true;
+        }
+        if(sub_mode.find("g") != std::string::npos){ // found gpu type
+            this->is_dynamic_device[gpu] = true;
+        }
+        if(sub_mode.find("t") != std::string::npos){ // found tpu type
+            this->is_dynamic_device[tpu] = true;
+        }
+    }// else: no partition mode(s). all static
+}
+
