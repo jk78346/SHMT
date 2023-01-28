115a7480853d0b258aaea8321b16eee87cdc1dca
khsu037 <jk78346@gmail.com>
2023-01-28 12:13:53 -0800

    kmeans' CPU version now is good at processing grayscale image to be consistent with other image type of benchmarks. While now CPU's quality is not consistent to itself due to some randomness from the quantized values of vector: centers. [Sol] Maybe to fix the quantized value by given k.

--- src/kernels/kernels_kmeans.cpp
+++ src/kernels/kernels_kmeans.cpp
@@ -2,48 +2,26 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
 
-cv::Mat vectorQuantization(cv::Mat vec,cv::Mat centers){
-	// accept only char type matrices
-	CV_Assert(vec.depth() != sizeof(uchar));
-	
-	int rows=vec.rows;
-	int cols=vec.cols;
-	cv::Mat quantize(rows,centers.cols,centers.type());
-	uchar *q=quantize.ptr<uchar>(0);
-	uchar *v=vec.ptr<uchar>(0);
-	uchar *c=centers.ptr<uchar>(0);
-	for(int i=0;i<rows;i++){
-	
-		q[3*i]=c[3*v[i]];
-		q[3*i+1]=c[3*v[i]+1];
-		q[3*i+2]=c[3*v[i]+1];
-	
-	}
-	return quantize;
-}
-
 void CpuKernel::kmeans_2d(const Mat in_img, Mat& out_img){
-    int k = 2;
-    cv::Mat labels, centers;
-    cv::Mat src_img = cv::imread("../data/lena.png");
-    cv::Mat src_reshaped = src_img.reshape(1, src_img.rows * src_img.cols);
-    src_reshaped.convertTo(src_reshaped, CV_32FC1);
-    cv::kmeans(src_reshaped, 
+    int k = 4;
+    std::vector<int> labels;
+    cv::Mat1f centers;
+    unsigned int size = in_img.rows * in_img.cols;
+    cv::Mat in_tmp = in_img.reshape(1, size);
+    in_tmp.convertTo(in_tmp, CV_32F);
+    cv::kmeans(in_tmp, // kmeans only takes CV_32F data type 
                k, 
                labels, 
                cv::TermCriteria(TermCriteria::MAX_ITER|TermCriteria::EPS, 
                                 10, // max iteration 
                                 1.0), // epsilon
-               3, // attempts
-               cv::KMEANS_RANDOM_CENTERS,
+               1, // attempts
+               cv::KMEANS_PP_CENTERS,
                centers);
-
-    centers.convertTo(centers,CV_8UC1);
-	labels.convertTo(labels,CV_8UC1);
-
-	cv::Mat quantized=vectorQuantization(labels,centers);
-	
-	quantized=quantized.reshape(3,src_img.rows);
-	quantized.convertTo(quantized,CV_8UC3);
-    cv::imwrite("../data/lena_kmeans_"+std::to_string(k)+".png", quantized);
+    for (unsigned int i = 0; i < size; i++) {
+        in_tmp.at<float>(i) = centers(labels[i]);
+    }
+    out_img = in_tmp.reshape(1, in_img.rows);
+    out_img.convertTo(out_img, CV_8U);
 }
+
