3b19e021a9c6edca1d939fd6622b069e6f4d2d5a
khsu037 <jk78346@gmail.com>
2023-01-30 20:48:15 -0800

    upload gptpu_utils aarch64 version of shared library

--- scripts/install_gptpu_utils_on_aarch64.sh
+++ scripts/install_gptpu_utils_on_aarch64.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-sudo cp ../edgetpu/out/aarch64/examples/libgptpu_utils.so /usr/local/lib/
-sudo cp ../edgetpu/src/cpp/examples/gptpu_utils.h /usr/local/include/
+
+# ===== use pre-compiled shared library =====
+sudo cp ../lib/aarch64/libgptpu_utils.so /usr/local/lib/
+sudo cp ../lib/aarch64/gptpu_utils.h /usr/local/include/
+
+# ===== if compile from edgetpu source =====
+#sudo cp ../edgetpu/out/aarch64/examples/libgptpu_utils.so /usr/local/lib/
+#sudo cp ../edgetpu/src/cpp/examples/gptpu_utils.h /usr/local/include/
